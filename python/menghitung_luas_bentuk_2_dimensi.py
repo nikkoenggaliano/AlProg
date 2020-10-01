99d438f99c50f891e6f03a843f77f01b139e45f2
Nikko Enggaliano <37658579+nikkoenggaliano@users.noreply.github.com>
2020-10-01 19:11:05 +0700

    fixing
    fixing idk what happend

--- python/menghitung_luas_bentuk_2_dimensi.py
+++ python/menghitung_luas_bentuk_2_dimensi.py
@@ -1,48 +1,49 @@
 def menu():
-print “Pilih Bentuk 2D”
-print
-print “1. Persegi Panjang”
-print “2. Lingkaran”
-print “3. Segitiga”
-print “4. Keluar”
+	print "Pilih Bentuk 2D"
+	print
+	print "1. Persegi Panjang"
+	print "2. Lingkaran"
+	print "3. Segitiga"
+	print "4. Keluar"
 
 def persegi():
-print “Menghitung Luas Persegi Panjang”
-p = input(“Masukkan Panjang : “)
-l = input(“Masukkan Lebar : “)
-luas = p*l
-print “Luas Persegi Panjang adalah “,luas
-print
-print “Coba lagi [Y/N]? ”
-back = raw_input().upper()
-if back == “Y”:
-menu()
-else:
-exit()
+	print "Menghitung Luas Persegi Panjang"
+	p = input("Masukkan Panjang : ")
+	l = input("Masukkan Lebar : ")
+	luas = p*l
+	print "Luas Persegi Panjang adalah ",luas
+	print
+	print "Coba lagi [Y/N]? "
+	back = raw_input().upper()
+	if back == "Y":
+		menu()
+	else:
+		exit()
 
 def lingkaran():
-print “Menghitung Luas Lingkaran”
-r = input(“Masukkan Jari-Jari : “)
-luas = 3.14*(r**2)
-print “Luas Lingkaran adalah “,luas
-print
-print “Coba lagi [Y/N]? ”
-back = raw_input().upper()
-if back == “Y”:
-menu()
-else:
-exit()
+	print "Menghitung Luas Lingkaran"
+	r = input("Masukkan Jari-Jari : ")
+	luas = 3.14*(r**2)
+	print "Luas Lingkaran adalah ",luas
+	print
+	print "Coba lagi [Y/N]? "
+	back = raw_input().upper()
+	if back == "Y":
+		menu()
+	else:
+		exit()
 
 def segitiga():
-print “Menghitung Luas Segitiga”
-a = input(“Masukkan Alas : “)
-t = input(“Masukkan Tinggi : “)
-luas = (a*t)/2
-print “Luas Segitiga adalah “,luas
-print
-print “Coba lagi [Y/N]? ”
-back = raw_input().upper()
-if back == “Y”:
-menu()
-else:
-exit()
+	print "Menghitung Luas Segitiga"
+	a = input("Masukkan Alas : ")
+	t = input("Masukkan Tinggi : ")
+	luas = (a*t)/2
+	print "Luas Segitiga adalah ",luas
+	print
+	print "Coba lagi [Y/N]? "
+	back = raw_input().upper()
+	if back == "Y":
+		menu()
+	else:
+		exit()
+
