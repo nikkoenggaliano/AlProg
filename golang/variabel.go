53b47a40d74446f40ebc32621b02109f7a033540
wadau <herlianali>
2020-10-01 09:08:19 +0700

    variabel pada go

---
+++ golang/variabel.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x float64 = 20.0
+
+	y := 42
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Printf("x adalah type %T\n", x)
+	fmt.Printf("y adalah type %T\n", y)
+}
