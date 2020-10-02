f8b7f715e4b129903a5eb8e9649679051946f082
XynSec <64978418+XynSec@users.noreply.github.com>
2020-10-02 09:02:51 +0700

    luassegitiga.py
    menghitung luas segitia

---
+++ python/luassegitiga.py
@@ -0,0 +1,5 @@
+print("Menghitung Luas Segitiga")
+alas = float(input("\nMasukan Alas : "))
+tinggi = float(input("Masukan Tinggi : "))
+luas = 0.5*alas*tinggi
+print("\nLuas Segitiga = %0.2f" % luas)
